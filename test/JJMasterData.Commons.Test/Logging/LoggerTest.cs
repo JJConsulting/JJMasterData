402248e704f3423b15b946c948d4743a3ceaee30
Lucio Pelinson <lucio@jjconsulting.com.br>
2022-10-20 19:18:45 -0300

    Fix stackoverflow exception when initialize without ConnectionString

---
+++ test/JJMasterData.Commons.Test/Logging/LoggerTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using JJMasterData.Commons.Language;
+using JJMasterData.Commons.Logging;
+
+namespace JJMasterData.Commons.Test.Logging;
+
+public class LoggerTest
+{
+    [Fact]
+    public void Log_Test()
+    {
+        Log.AddInfo("Test");
+        Assert.True(true);
+    }
+
+}
