770cbd99165887fe9ac9a6c99674986c307bac79
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2022-11-07 16:49:10 -0300

    Removed JJMasterData.Pdf

--- src/Plugins/JJMasterData.Pdf/Extensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using JJMasterData.Commons.DI;
-using JJMasterData.Core.Extensions;
-
-namespace JJMasterData.Pdf;
-public static class Extensions
-{
-    public static JJServiceBuilder WithPdfExportation(this JJServiceBuilder builder) => builder.WithPdfExportation<PdfWriter>();
-}
