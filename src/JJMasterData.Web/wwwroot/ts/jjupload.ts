96518a38e1ebd2c81edf8a1ed6a013d6a33d689a
Lucio Pelinson <lucio@jjconsulting.com.br>
2022-11-10 18:12:47 -0300

    layout bs5 fix

--- src/JJMasterData.Web/wwwroot/ts/jjupload.ts
+++ src/JJMasterData.Web/wwwroot/ts/jjupload.ts
@@ -39,7 +39,7 @@
                 dragdropWidth: ($(this).width() - 10),
                 statusBarWidth: ($(this).width() - 10),
                 autoSubmit: true,
-                uploadButtonClass: "btn btn-default",
+                uploadButtonClass: "btn btn-primary",
                 allowedTypes: allowedTypes,
                 acceptFiles: acceptFiles,
                 uploadStr: obj.attr("uploadStr"),
