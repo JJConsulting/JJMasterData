44a39179986182132333f0da2e7709a58ce025f7
Lucio Pelinson <lucio.pelinson@gmail.com>
2022-11-23 01:29:30 -0300

    bug fix

--- test/JJMasterData.Api.Test/Controllers/DictionariesControllerTest.cs
+++ test/JJMasterData.Api.Test/Controllers/DictionariesControllerTest.cs
@@ -1,6 +1,9 @@
 using JJMasterData.Api.Controllers;
 using JJMasterData.Api.Services;
+using JJMasterData.Commons.Dao;
+using JJMasterData.Commons.DI;
 using JJMasterData.Core.DataDictionary;
+using JJMasterData.Core.DataDictionary.Repository;
 using Xunit.Extensions.Ordering;
 
 namespace JJMasterData.Api.Test.Controllers;
@@ -12,7 +15,11 @@ public class DictionariesControllerTest
     
     public DictionariesControllerTest()
     {
-        _controller = new DictionariesController(new AccountService());
+        IEntityRepository entityRepository = JJService.EntityRepository; 
+        IDictionaryRepository dictionaryRepository = new DictionaryDao(entityRepository);
+        var dictionariesService = new DictionariesService(dictionaryRepository, entityRepository);
+        var accountService = new AccountService();
+        _controller = new DictionariesController(accountService, dictionariesService, dictionaryRepository);
     }
     
     [Fact]
