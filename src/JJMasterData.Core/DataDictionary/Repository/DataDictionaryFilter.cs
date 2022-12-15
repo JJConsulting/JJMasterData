4cebddad8ac14821b85e989bdb493d4aae606812
Lucio Pelinson <lucio.pelinson@gmail.com>
2022-12-14 23:20:56 -0300

    Removing unnecessary docs

--- src/JJMasterData.Core/DataDictionary/Repository/DataDictionaryFilter.cs
+++ src/JJMasterData.Core/DataDictionary/Repository/DataDictionaryFilter.cs
@@ -5,9 +5,6 @@ using System.Linq;
 
 namespace JJMasterData.Core.DataDictionary.Repository;
 
-/// <summary>
-/// The Data Dictionaries (metadata) are stored in the Database
-/// </summary>
 public class DataDictionaryFilter
 {
     public string Name { get; set; }
