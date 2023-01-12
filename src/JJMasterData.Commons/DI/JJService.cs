3ca8f65855c35b45cdf9fc4c15952ce54a9290ec
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2023-01-12 17:44:26 -0300

    BugFix & HtmlAgilityPack at tests

--- src/JJMasterData.Commons/DI/JJService.cs
+++ src/JJMasterData.Commons/DI/JJService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using JJMasterData.Commons.Dao;
 using JJMasterData.Commons.Language;
-using JJMasterData.Commons.Logging;
-using JJMasterData.Commons.Logging.Db;
 using JJMasterData.Commons.Options;
 using JJMasterData.Commons.Tasks;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +9,10 @@ using Microsoft.Extensions.Options;
 
 namespace JJMasterData.Commons.DI;
 
+/// <summary>
+/// Static service locator. We're aware this is a anti-pattern, this was made to prevent breaking changes at .NET Framework 4.8.
+/// This class will be removed at the next version, DON'T USE at your applications, always use constructor injector.
+/// </summary>
 public static class JJService
 {
     public static IServiceProvider Provider { get; internal set; }
