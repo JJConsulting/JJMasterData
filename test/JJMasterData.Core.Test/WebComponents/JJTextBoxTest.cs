dca679bc12370458088e1236bbb459facc2f8e44
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2023-01-16 13:53:53 -0300

    Revert HtmlAgilityPack

--- test/JJMasterData.Core.Test/WebComponents/JJTextBoxTest.cs
+++ test/JJMasterData.Core.Test/WebComponents/JJTextBoxTest.cs
@@ -1,38 +1,64 @@
 ﻿using JJMasterData.Core.WebComponents;
-using HtmlAgilityPack;
+using System.Text;
 
-namespace JJMasterData.Core.Test.WebComponents;
-
-public class JJTextBoxTest
+namespace JJMasterData.Core.Test.WebComponents
 {
-    [Fact]
-    public void ToStringTest()
+    public class JJTextBoxTest
     {
-        var textBox = GetTextBox();
-        var html = textBox.GetHtmlBuilder();
-
-        var document = new HtmlDocument();
-        document.LoadHtml(html.ToString(true));
-
-        Assert.NotNull(document.GetElementbyId("textBoxId"));
-        Assert.Contains(document.DocumentNode.ChildNodes, node => node.HasClass("class1"));
-        Assert.Contains(document.DocumentNode.ChildNodes, node => node.HasClass("class2"));
-        Assert.Contains(document.DocumentNode.ChildNodes, node => node.GetAttributeValue("value", null) == "My Text");
-        Assert.Contains(document.DocumentNode.ChildNodes, node => node.GetAttributeValue("key", null) == "value");
-    }
-
-    private static JJTextBox GetTextBox()
-    {
-        return new JJTextBox()
+        [Fact]
+        public void RenderTest()
         {
-            Name = "textBoxId",
-            ToolTip = "textBoxTest",
-            Text = "My Text",
-            CssClass = "class1 class2",
-            Attributes =
+            var text = GetTextBox();
+            var expected = new StringBuilder();
+            expected.AppendLine().Append(' ', 2);
+            expected.Append("<input ");
+            expected.Append("id=\"id1\" ");
+            expected.Append("name=\"id1\" ");
+            expected.Append("pan=\"pan\" ");
+            expected.Append("placeholder=\"00\" ");
+            expected.Append("type=\"text\" ");
+            expected.Append("class=\"form-control class1 class2\" ");
+            expected.Append("title=\"teste\" ");
+            expected.Append("data-bs-toggle=\"tooltip\" ");
+            expected.Append("value=\"1188880000\" ");
+            expected.Append("/>");
+
+            var html = text.GetHtmlBuilder();
+            Assert.Equal(expected.ToString(), html.ToString(true));
+        }
+
+        [Fact]
+        public void RenderTestAttrs()
+        {
+            var text = GetTextBox();
+            var actual = text.GetHtmlBuilder();
+
+            Assert.Equal(text.Name, actual.GetAttribute("name"));
+            Assert.Equal(text.Name, actual.GetAttribute("id"));
+            Assert.Equal("pan", actual.GetAttribute("pan"));
+            Assert.Equal("00", actual.GetAttribute("placeholder"));
+            Assert.Equal("text", actual.GetAttribute("type"));
+            Assert.Equal("form-control class1 class2", actual.GetAttribute("class"));
+            Assert.Equal("teste", actual.GetAttribute("title"));
+            Assert.Equal("tooltip", actual.GetAttribute("data-bs-toggle"));
+            Assert.Equal("1188880000", actual.GetAttribute("value"));
+        }
+
+
+        private JJTextBox GetTextBox()
+        {
+            return new JJTextBox
             {
-                { "key", "value" }
-            }
-        };
+                Name = "id1",
+                ToolTip = "teste",
+                Text = "1188880000",
+                CssClass = "class1 class2",
+                PlaceHolder = "00",
+                Attributes =
+                {
+                    { "pan", "pan" }
+                }
+            };
+        }
     }
 }
