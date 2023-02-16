b226699c24ca65384a94f26b6a93992a22fe08b2
gumbarros <gustavomauriciodebarros@gmail.com>
2023-02-16 09:56:29 -0300

    Added async overloads to DataAccessExtensions.cs

--- src/JJMasterData.Commons/Extensions/DataAccessExtensions.cs
+++ src/JJMasterData.Commons/Extensions/DataAccessExtensions.cs
@@ -1,27 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 using JJMasterData.Commons.Data;
 
 namespace JJMasterData.Commons.Extensions;
 
 public static class DataAccessExtensions
 {
-    public static T GetModel<T>(this DataAccess dataAccess, string sql)
-    {
-        return dataAccess.GetFields(sql).ToModel<T>();
-    }
-
+    
     public static T GetModel<T>(this DataAccess dataAccess, DataAccessCommand cmd)
     {
         return dataAccess.GetFields(cmd).ToModel<T>();
     }
+    
 
-    public static List<T> GetModelList<T>(this DataAccess dataAccess, string sql)
+    public static async Task<T> GetModelAsync<T>(this DataAccess dataAccess, DataAccessCommand cmd)
     {
-        return dataAccess.GetDataTable(sql).ToModelList<T>();
+        var result = await dataAccess.GetFieldsAsync(cmd);
+        return result.ToModel<T>();
     }
-
-    public static List<T> GetModelList<T>(this DataAccess dataAccess, DataAccessCommand cmd)
+    
+    public static IList<T> GetModelList<T>(this DataAccess dataAccess, DataAccessCommand cmd)
     {
         return dataAccess.GetDataTable(cmd).ToModelList<T>();
+    }
+    
+    public static async IAsyncEnumerable<T> GetModelAsyncEnumerable<T>(this DataAccess dataAccess, DataAccessCommand cmd)
+    {
+        var dataTable = await dataAccess.GetDataTableAsync(cmd);
+        var result = dataTable.ToModelList<T>();
+        
+        foreach (var model in result)
+            yield return model;
     }
 }
