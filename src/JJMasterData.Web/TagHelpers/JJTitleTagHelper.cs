0eb08aa5f97f31dc7a12278d09d25f890692b9e5
ygor.silva <ygor.silva@jjconsulting.combr>
2023-03-01 15:06:31 -0300

    add size parameter to jjtitle

--- src/JJMasterData.Web/TagHelpers/JJTitleTagHelper.cs
+++ src/JJMasterData.Web/TagHelpers/JJTitleTagHelper.cs
@@ -8,17 +8,25 @@ public class JJTitleTagHelper : TagHelper
 {
     [HtmlAttributeName("title")]
     public string? Title { get; set; }
-    
+
     [HtmlAttributeName("subtitle")]
     public string? SubTitle { get; set; }
-    
+
+    [HtmlAttributeName("size")]
+    public HeadingSize? Size { get; set; }
+
     public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
         var title = new JJTitle(Title ?? string.Empty, SubTitle ?? string.Empty);
-        
+
+        if (Size is not null)
+        {
+            title.Size = Size.Value;
+        }
+
         output.TagMode = TagMode.StartTagAndEndTag;
         output.Content.SetHtmlContent(title.GetHtml());
-        
+
         return Task.CompletedTask;
     }
 }
