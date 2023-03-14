c5c0f650eca47b3f5766ead89068e5d2c0b0b0ae
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2023-03-14 11:26:13 -0300

    Update NuGet version

--- src/JJMasterData.Web/TagHelpers/JJValidationSummaryTagHelper.cs
+++ src/JJMasterData.Web/TagHelpers/JJValidationSummaryTagHelper.cs
@@ -6,13 +6,13 @@ namespace JJMasterData.Web.TagHelpers;
 public class JJValidationSummaryTagHelper : TagHelper
 {
     [HtmlAttributeName("errors")] 
-    public IEnumerable<string> Errors { get; set; }
+    public IEnumerable<string>? Errors { get; set; }
 
     public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
-        var validationSummary = new JJValidationSummary() 
+        var validationSummary = new JJValidationSummary
         {
-            Errors = Errors.ToList()
+            Errors = Errors?.ToList()
         };
         output.TagMode = TagMode.StartTagAndEndTag;
         output.Content.SetHtmlContent(validationSummary.GetHtml());
