4fb6692b95e88c5b04352114ea6d9905b0e77269
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2023-03-28 09:03:58 -0300

    Fix DataMember name

--- src/JJMasterData.Core/DataDictionary/FormElementDataItem.cs
+++ src/JJMasterData.Core/DataDictionary/FormElementDataItem.cs
@@ -69,9 +69,9 @@ public class FormElementDataItem
     public bool ReplaceTextOnGrid { get; set; }
 
     /// <remarks>
-    /// Be careful when using this option, you will maybe use this option to WriteOnly fields or store the values in another table.
+    /// Be careful when using this option. You should probably use this option only for WriteOnly fields or store the values in another table.
     /// </remarks>
-    [DataMember(Name = "enableMultipleValues")]
+    [DataMember(Name = "enableMultiSelect")]
     public bool EnableMultiSelect { get; set; }
     
     /// <summary>
