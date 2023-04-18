c8dd630a68f2a283c4416f58393f8fe5d86247ed
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2023-04-18 08:52:27 -0300

    Improve logging performance using LongRunning tasks (#110)

--- src/JJMasterData.Commons/Logging/File/FileLogger.cs
+++ src/JJMasterData.Commons/Logging/File/FileLogger.cs
@@ -1,18 +1,17 @@
 using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using JJMasterData.Commons.Util;
 using Microsoft.Extensions.Logging;
-
 
 namespace JJMasterData.Commons.Logging.File;
 
 public class FileLogger : ILogger
 {
     private readonly FileLoggerProvider _fileLoggerProvider;
-    private static readonly object locker = new();
+    private readonly BlockingCollection<LogMessage> _queue;
 
     /// <summary>
     /// Creates a new instance of <see cref="FileLogger" />.
@@ -20,6 +19,8 @@ public class FileLogger : ILogger
     public FileLogger(FileLoggerProvider fileLoggerProvider)
     {
         _fileLoggerProvider = fileLoggerProvider;
+        _queue = new BlockingCollection<LogMessage>();
+        Task.Factory.StartNew(LogAtFile, TaskCreationOptions.LongRunning);
     }
 
     public IDisposable BeginScope<TState>(TState state) => default!;
@@ -45,44 +46,49 @@ public class FileLogger : ILogger
     /// <param name="formatter">A delegate that formats </param>
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
     {
-
         if (!IsEnabled(logLevel))
             return;
 
-        var path = FileIO.ResolveFilePath(_fileLoggerProvider.Options.CurrentValue.FileName);
-        var directory = Path.GetDirectoryName(path);
+        var message = new LogMessage(logLevel, eventId, state!, exception, (s, e) => formatter((TState)s, e));
+        _queue.Add(message);
+    }
 
-        if (!Directory.Exists(directory))
-            Directory.CreateDirectory(directory!);
-
-        string record = GetLogRecord(logLevel, eventId.Name, formatter(state, exception), exception);
-
-        lock (locker)
+    private void LogAtFile()
+    {
+        foreach (var message in _queue.GetConsumingEnumerable())
         {
+            var path = FileIO.ResolveFilePath(_fileLoggerProvider.Options.CurrentValue.FileName);
+            var directory = Path.GetDirectoryName(path);
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory!);
+
+            var record = GetLogRecord(message);
+            
             using var writer = new StreamWriter(path, true);
             writer.Write(record);
         }
     }
 
-    private string GetLogRecord(LogLevel logLevel, string eventName, string message, Exception exception)
+    private static string GetLogRecord(LogMessage message)
     {
         var log = new StringBuilder();
 
         log.AppendFormat("{0:yyyy-MM-dd HH:mm:ss+00:00} -", DateTime.Now);
-        log.AppendFormat(" [{0}] ", logLevel);
+        log.AppendFormat(" [{0}] ", message.LogLevel);
 
-        if (!string.IsNullOrWhiteSpace(eventName))
+        if (!string.IsNullOrWhiteSpace(message.EventId.Name))
         {
-            log.AppendFormat(" [{0}] ", eventName);
+            log.AppendFormat(" [{0}] ", message.EventId.Name);
         }
 
         log.AppendFormat(" {0} ", message);
 
-        if (exception != null)
+        if (message.Exception != null)
         {
-            log.AppendLine(exception.Message);
-            log.AppendLine(exception.StackTrace);
-            log.AppendFormat("Source: {0}", exception.Source);
+            log.AppendLine(message.Exception.Message);
+            log.AppendLine(message.Exception.StackTrace);
+            log.AppendFormat("Source: {0}", message.Exception.Source);
         }
 
         log.AppendLine();
