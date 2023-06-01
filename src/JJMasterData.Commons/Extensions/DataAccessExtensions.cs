0eedf9a827ddbbf046c600bcc346d011443ccc5e
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2023-06-01 16:55:29 -0300

    Added JsonSerializerSettings to ToModel methods (#126)

--- src/JJMasterData.Commons/Extensions/DataAccessExtensions.cs
+++ src/JJMasterData.Commons/Extensions/DataAccessExtensions.cs
@@ -1,41 +1,48 @@
-﻿using System.Collections;
+﻿#nullable enable
 using System.Collections.Generic;
-using System.Data;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using JJMasterData.Commons.Data;
+using Newtonsoft.Json;
 
 namespace JJMasterData.Commons.Extensions;
 
 public static class DataAccessExtensions
 {
-    
-    public static T GetModel<T>(this DataAccess dataAccess, DataAccessCommand cmd)
+    public static T? GetModel<T>(this DataAccess dataAccess, DataAccessCommand cmd,
+        JsonSerializerSettings? serializerSettings = null)
     {
-        return dataAccess.GetFields(cmd).ToModel<T>();
+        return dataAccess.GetFields(cmd).ToModel<T>(serializerSettings);
     }
 
-    public static async Task<T> GetModelAsync<T>(this DataAccess dataAccess, DataAccessCommand cmd, CancellationToken cancellationToken = default)
+    public static async Task<T?> GetModelAsync<T>(this DataAccess dataAccess, DataAccessCommand cmd,
+        JsonSerializerSettings? serializerSettings = null, CancellationToken cancellationToken = default)
     {
         var result = await dataAccess.GetFieldsAsync(cmd, cancellationToken);
-        return result.ToModel<T>();
+        return result.ToModel<T>(serializerSettings);
     }
-    
-    public static IList<T> GetModelList<T>(this DataAccess dataAccess, DataAccessCommand cmd)
+
+    public static IList<T>? GetModelList<T>(this DataAccess dataAccess, DataAccessCommand cmd,
+        JsonSerializerSettings? serializerSettings = null)
     {
-        return dataAccess.GetDataTable(cmd).ToModelList<T>();
+        return dataAccess.GetDataTable(cmd).ToModelList<T>(serializerSettings);
     }
-    
-    public static async IAsyncEnumerable<T> GetModelAsyncEnumerable<T>(this DataAccess dataAccess, DataAccessCommand cmd, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+
+    public static async IAsyncEnumerable<T>? GetModelAsyncEnumerable<T>(this DataAccess dataAccess,
+        DataAccessCommand cmd, JsonSerializerSettings? serializerSettings = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         var dataTable = await dataAccess.GetDataTableAsync(cmd, cancellationToken);
-        var result = dataTable.ToModelList<T>();
+        var result = dataTable.ToModelList<T>(serializerSettings);
 
-        foreach (var model in result)
+        if (result != null)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            yield return model;
+            foreach (var model in result)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return model;
+            }
         }
     }
 }
