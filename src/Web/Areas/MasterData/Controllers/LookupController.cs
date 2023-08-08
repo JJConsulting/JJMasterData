183cae5c323b24a88d9fc751ddcf4c19c697828f
Lucio Pelinson <lucio@jjconsulting.com.br>
2023-08-08 16:04:52 -0300

    sync

--- src/Web/Areas/MasterData/Controllers/LookupController.cs
+++ src/Web/Areas/MasterData/Controllers/LookupController.cs
@@ -3,12 +3,9 @@ using JJMasterData.Core.DataDictionary;
 using JJMasterData.Core.DataDictionary.Actions.Abstractions;
 using JJMasterData.Core.DataDictionary.Actions.GridToolbar;
 using JJMasterData.Core.DataDictionary.Actions.UserCreated;
-using JJMasterData.Core.DataDictionary.Repository.Abstractions;
 using JJMasterData.Core.DataManager;
 using JJMasterData.Core.DataManager.Services;
-using JJMasterData.Core.UI.Components;
 using JJMasterData.Core.Web.Components;
-using JJMasterData.Core.Web.Factories;
 using JJMasterData.Web.Areas.MasterData.Models;
 using JJMasterData.Web.Filters;
 using Microsoft.AspNetCore.Mvc;
@@ -18,27 +15,21 @@ namespace JJMasterData.Web.Areas.MasterData.Controllers;
 
 public class LookupController : MasterDataController
 {
-    private  IFormElementComponentFactory<JJFormView> FormViewFactory { get; }
-    private IDataDictionaryRepository DataDictionaryRepository { get; }
     private ILookupService LookupService { get; }
     private IFormValuesService FormValuesService { get; }
     private IStringLocalizer<JJMasterDataResources> StringLocalizer { get; }
 
     public LookupController(
-        IFormElementComponentFactory<JJFormView> formViewFactory,
-        IDataDictionaryRepository dataDictionaryRepository,
         ILookupService lookupService,
         IFormValuesService formValuesService,
         IStringLocalizer<JJMasterDataResources> stringLocalizer
         )
     {
-        FormViewFactory = formViewFactory;
-        DataDictionaryRepository = dataDictionaryRepository;
         LookupService = lookupService;
         FormValuesService = formValuesService;
         StringLocalizer = stringLocalizer;
     }
-    
+
     [ServiceFilter<LookupParametersDecryptionFilter>]
     public IActionResult Index(LookupParameters lookupParameters)
     {
@@ -46,24 +37,22 @@ public class LookupController : MasterDataController
         {
             LookupParameters = lookupParameters,
             LookupFormConfiguration = ConfigureLookupForm
-        }); 
+        });
     }
 
     [ServiceFilter<FormElementDecryptionFilter>]
     public async Task<IActionResult> GetResult(
-        FormElement formElement, 
+        FormElement formElement,
         string componentName,
         PageState pageState,
         string searchId)
     {
         var dataItem = formElement.Fields[componentName].DataItem;
-
-        var formValues = await FormValuesService.GetFormValuesWithMergedValuesAsync(formElement,pageState,true);
-
+        var formValues = await FormValuesService.GetFormValuesWithMergedValuesAsync(formElement, pageState, true);
+        var formStateData = new FormStateData(formValues, pageState);
         var selectedValue = LookupService.GetSelectedValue(componentName).ToString();
-        
-        var description = await LookupService.GetDescriptionAsync(dataItem,selectedValue,pageState,formValues,false);
-        return Json(new LookupResultDto(searchId,description));
+        var description = await LookupService.GetDescriptionAsync(dataItem, formStateData, selectedValue, false);
+        return Json(new LookupResultDto(searchId, description));
     }
 
     private void ConfigureLookupForm(JJFormView form, LookupParameters lookupParameters)
@@ -108,10 +97,10 @@ public class LookupController : MasterDataController
 
     private static Func<BasicAction, bool> IsLookupAction()
     {
-        return action => action is not LegendAction 
-                         && action is not RefreshAction 
-                         && action is not FilterAction 
-                         && action is not ConfigAction 
+        return action => action is not LegendAction
+                         && action is not RefreshAction
+                         && action is not FilterAction
+                         && action is not ConfigAction
                          && action is not SortAction;
     }
 }
