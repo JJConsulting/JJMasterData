e6cfef765cbb910daca46d4f8751d18efef70447
Lucio Pelinson <lucio.pelinson@gmail.com>
2023-08-19 11:59:33 -0300

    bugfix: TextArea rows

--- src/Core/UI/Components/Controls/TextArea/JJTextArea.cs
+++ src/Core/UI/Components/Controls/TextArea/JJTextArea.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using JJMasterData.Commons.Localization;
-using JJMasterData.Core.DataDictionary;
+﻿using JJMasterData.Commons.Localization;
 using JJMasterData.Core.UI.Components.Controls;
 using JJMasterData.Core.Web.Html;
 using JJMasterData.Core.Web.Http.Abstractions;
@@ -29,6 +27,7 @@ public class JJTextArea : HtmlControl
             .WithToolTip(ToolTip)
             .WithAttributeIf(!string.IsNullOrWhiteSpace(PlaceHolder), "placeholder", PlaceHolder)
             .WithAttribute("cols", "20")
+            .WithAttribute("rows", Rows)
             .WithAttribute("maximum-limit-of-characters-label", StringLocalizer["Maximum limit of {0} characters!"])
             .WithAttribute("characters-remaining-label", StringLocalizer["({0} characters remaining)"])
             .WithAttributeIf(MaxLength > 0, "maxlength", MaxLength.ToString())
