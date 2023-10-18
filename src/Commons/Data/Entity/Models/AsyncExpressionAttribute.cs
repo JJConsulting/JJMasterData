728e9b75c6bbcfbdfcccc7b38e6688c86a031ebb
gumbarros <gustavomauriciodebarros@gmail.com>
2023-10-18 15:35:47 -0300

    Remove redundant AttributeUsage

--- src/Commons/Data/Entity/Models/AsyncExpressionAttribute.cs
+++ src/Commons/Data/Entity/Models/AsyncExpressionAttribute.cs
@@ -1,8 +1,5 @@
-using System;
-
 namespace JJMasterData.Commons.Data.Entity.Models;
 
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter)]
 public class AsyncExpressionAttribute : ExpressionAttribute
 {
     
