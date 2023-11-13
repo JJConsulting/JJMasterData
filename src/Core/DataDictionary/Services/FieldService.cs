e59f586d3dfb4aa33e3c4c85f9b1653f13ddff4a
gumbarros <gustavomauriciodebarros@gmail.com>
2023-11-13 14:53:54 -0300

    Fix typo

--- src/Core/DataDictionary/Services/FieldService.cs
+++ src/Core/DataDictionary/Services/FieldService.cs
@@ -7,7 +7,7 @@ using JJMasterData.Commons.Localization;
 using JJMasterData.Core.DataDictionary.Models;
 using JJMasterData.Core.DataDictionary.Repository.Abstractions;
 using JJMasterData.Core.DataManager.Expressions.Abstractions;
-using JJMasterData.Core.DataManager.Expressions.Extensions;
+using JJMasterData.Core.Extensions;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Localization;
 
