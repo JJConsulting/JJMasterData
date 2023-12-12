63995336fd50c86b304e3002dc10b42bd17f7a98
gumbarros <gustavomauriciodebarros@gmail.com>
2023-12-12 09:33:44 -0300

    Added missing UserValues parameter at LookupController

--- src/Web/Areas/MasterData/Controllers/LookupController.cs
+++ src/Web/Areas/MasterData/Controllers/LookupController.cs
@@ -59,7 +59,7 @@ public class LookupController(
         PageState pageState)
     {
         var elementMap = formElement.Fields[fieldName].DataItem!.ElementMap;
-        var formValues = await FormValuesService.GetFormValuesWithMergedValuesAsync(formElement, pageState, true);
+        var formValues = await FormValuesService.GetFormValuesWithMergedValuesAsync(formElement, pageState,new Dictionary<string, object?>(), true);
         var formStateData = new FormStateData(formValues, pageState);
         var selectedValue = LookupService.GetSelectedValue(componentName);
         
