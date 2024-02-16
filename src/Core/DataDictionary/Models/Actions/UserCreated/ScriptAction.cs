5f0f8beef338795c76c7e8f2ddc9d28fadee8144
gumbarros <gustavomauriciodebarros@gmail.com>
2024-02-16 14:43:59 -0300

    Add font-monospace and l10n to ScriptAction.cshtml

--- src/Core/DataDictionary/Models/Actions/UserCreated/ScriptAction.cs
+++ src/Core/DataDictionary/Models/Actions/UserCreated/ScriptAction.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace JJMasterData.Core.DataDictionary.Models.Actions;
 
@@ -8,6 +9,7 @@ public class ScriptAction : UserCreatedAction
     /// JS script that will be executed when the user clicks on the control.
     /// </summary>
     [JsonProperty("onClientClick")]
+    [Display(Name = "Script")]
     // ReSharper disable once InconsistentNaming
     public string OnClientClick { get; set; }
     public override bool IsUserCreated => true;
