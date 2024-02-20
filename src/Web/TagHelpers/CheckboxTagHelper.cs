f0bccc1e04c7489fdabd32895a8154c1536a7c78
gumbarros <gustavomauriciodebarros@gmail.com>
2024-02-20 15:07:42 -0300

    bugfix at CheckboxTagHelper.cs: Correctly recover name from ViewContext

--- src/Web/TagHelpers/CheckboxTagHelper.cs
+++ src/Web/TagHelpers/CheckboxTagHelper.cs
@@ -1,11 +1,13 @@
+using JetBrains.Annotations;
 using JJMasterData.Commons.Exceptions;
 using JJMasterData.Core.UI.Components;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace JJMasterData.Web.TagHelpers;
 
-public class CheckboxTagHelper(IControlFactory<JJCheckBox> checkboxFactory) : TagHelper
+public class CheckboxTagHelper(IControlFactory<JJCheckBox> checkboxFactory, IHtmlHelper htmlHelper) : TagHelper 
 {
     private IControlFactory<JJCheckBox> CheckboxFactory { get; set; } = checkboxFactory;
 
@@ -33,12 +35,16 @@ public class CheckboxTagHelper(IControlFactory<JJCheckBox> checkboxFactory) : Ta
     [HtmlAttributeName("onchange")] 
     public string? OnChange { get; set; }
 
+    [ViewContext] [HtmlAttributeNotBound] 
+    public ViewContext ViewContext { get; set; } = null!;
+    
     public bool Enabled { get; set; } = true;
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
+        Contextualize(ViewContext);
         var checkBox = CheckboxFactory.Create();
-        checkBox.Name = Name ?? For?.Name ?? throw new JJMasterDataException("Either for or name attributes are required.");
+        checkBox.Name = Name ?? htmlHelper.Name(For!.Name) ?? throw new JJMasterDataException("Either for or name attributes are required.");
         checkBox.Enabled = Enabled;
         if (For is not null)
         {
@@ -50,9 +56,7 @@ public class CheckboxTagHelper(IControlFactory<JJCheckBox> checkboxFactory) : Ta
         }
 
         if (OnChange is not null)
-        {
             checkBox.Attributes["onchange"] = OnChange;
-        }
         
         checkBox.IsSwitch = IsSwitch;
         checkBox.SwitchSize = SwitchSize ?? CheckBoxSwitchSize.Medium;
@@ -65,5 +69,12 @@ public class CheckboxTagHelper(IControlFactory<JJCheckBox> checkboxFactory) : Ta
         var htmlBuilder = checkBox.GetHtmlBuilder();
         
         output.Content.SetHtmlContent(htmlBuilder.ToString());
+    }
+
+    public void Contextualize(ViewContext viewContext)
+    {
+        if (htmlHelper is IViewContextAware aware) {
+            aware.Contextualize(viewContext);
+        }
     }
 }
