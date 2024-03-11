b573af8107d1c586289ac8209ed66ba79fb99460
gumbarros <gustavomauriciodebarros@gmail.com>
2024-03-11 11:28:54 -0300

    l10n

--- src/Web/Areas/DataDictionary/Models/ViewModels/AddElementViewModel.cs
+++ src/Web/Areas/DataDictionary/Models/ViewModels/AddElementViewModel.cs
@@ -5,6 +5,6 @@ namespace JJMasterData.Web.Areas.DataDictionary.Models.ViewModels;
 public class AddElementViewModel
 {
     [Display(Name = "Table Or View Name", Prompt = "Table Or View Name")]
-    public string Name { get; set; } = null!;
-    public bool ImportFields { get; set; }
+    public string Name { get; init; } = null!;
+    public bool ImportFields { get; init; }
 }
--- src/Web/Areas/DataDictionary/Models/ViewModels/DuplicateElementViewModel.cs
+++ src/Web/Areas/DataDictionary/Models/ViewModels/DuplicateElementViewModel.cs
@@ -5,7 +5,7 @@ namespace JJMasterData.Web.Areas.DataDictionary.Models.ViewModels;
 public class DuplicateElementViewModel
 {
     [Display(Name="Original Element Name", Prompt = "Original Element Name")]
-    public string? OriginalElementName { get; set; } 
+    public string? OriginalElementName { get; init; } 
     [Display(Name="New Element Name", Prompt = "New Element Name")]
-    public string? NewElementName { get; set; }
+    public string? NewElementName { get; init; }
 }
