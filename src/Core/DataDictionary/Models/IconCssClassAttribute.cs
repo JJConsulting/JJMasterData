763347342bf4b50f137c4b6ff4e3522c2f54560a
gumbarros <gustavomauriciodebarros@gmail.com>
2024-03-20 13:28:56 -0300

    Change attribute to internal sealed.

--- src/Core/DataDictionary/Models/IconCssClassAttribute.cs
+++ src/Core/DataDictionary/Models/IconCssClassAttribute.cs
@@ -3,7 +3,7 @@
 namespace JJMasterData.Core.DataDictionary.Models;
 
 [AttributeUsage(AttributeTargets.Field)]
-public class IconCssClassAttribute(string cssClass) : Attribute
+internal sealed class IconCssClassAttribute(string cssClass) : Attribute
 {
     public string CssClass { get; } = cssClass;
 }
