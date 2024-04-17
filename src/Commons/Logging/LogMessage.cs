c80ce8e176bd6b755a54da603d90cbe75dea1d23
gumbarros <gustavomauriciodebarros@gmail.com>
2024-04-17 10:48:37 -0300

    Added readonly modifier

--- src/Commons/Logging/LogMessage.cs
+++ src/Commons/Logging/LogMessage.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace JJMasterData.Commons.Logging;
 
-public struct LogMessage
+public readonly struct LogMessage
 {
     public required DateTime Created { get; init; }
     public required int LogLevel { get; init; }
