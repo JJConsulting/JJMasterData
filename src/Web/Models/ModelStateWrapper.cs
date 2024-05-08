b590b35983df29db2168ea07f5acce9f5134b7bc
gumbarros <gustavomauriciodebarros@gmail.com>
2024-05-08 11:24:16 -0300

    `ModelStateWrapper` now allow empty `ModelStateDictionary`

--- src/Web/Models/ModelStateWrapper.cs
+++ src/Web/Models/ModelStateWrapper.cs
@@ -6,7 +6,7 @@ namespace JJMasterData.Web.Models;
 
 public class ModelStateWrapper(IActionContextAccessor actionContextAccessor) : IValidationDictionary
 {
-    private readonly ModelStateDictionary _modelState = actionContextAccessor.ActionContext!.ModelState;
+    private readonly ModelStateDictionary _modelState = actionContextAccessor.ActionContext?.ModelState ?? new ModelStateDictionary();
 
     public IEnumerable<string> Errors => 
         _modelState.Values.SelectMany(entry => entry.Errors.Select(e => e.ErrorMessage));
