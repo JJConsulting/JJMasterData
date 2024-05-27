af36f03aacdfb01b4dd493d0e3ccdf6176b68323
gumbarros <gustavomauriciodebarros@gmail.com>
2024-05-27 09:27:51 -0300

    Remove More from `_Menu`

--- src/Web/TagHelpers/BreadcrumbTagHelper.cs
+++ src/Web/TagHelpers/BreadcrumbTagHelper.cs
@@ -8,10 +8,15 @@ public class BreadcrumbTagHelper(HtmlComponentFactory htmlComponentFactory) : Ta
     [HtmlAttributeName("items")]
     public List<BreadcrumbItem> Items { get; set; } = null!;
     
+    [HtmlAttributeName("css-class")]
+    public string? CssClass { get; set; } 
+    
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         var breadcrumb = htmlComponentFactory.Breadcrumb.Create(Items);
         
+        if(CssClass is not null)
+            breadcrumb.CssClass = CssClass;
         output.SuppressOutput();
         output.Content.SetHtmlContent(breadcrumb.GetHtml());
         
