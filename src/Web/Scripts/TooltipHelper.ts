7566b0338ce5f6bf1105c70bb887b8e049e87d00
gumbarros <gustavomauriciodebarros@gmail.com>
2024-06-06 20:04:30 -0300

    Add `createTooltip` method

--- src/Web/Scripts/TooltipHelper.ts
+++ src/Web/Scripts/TooltipHelper.ts
@@ -11,4 +11,10 @@ class TooltipHelper {
         const tooltipTriggerList = document.querySelectorAll(selectorPrefix + ' [data-bs-toggle="tooltip"]')
         tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl,{   trigger : 'hover'}))
     }
+    
+    static createTooltip(id){
+        document.addEventListener("DOMContentLoaded", function (){
+            new bootstrap.Tooltip(document.getElementById(id),{ trigger : 'hover'})
+        });
+    }
 }
--- src/Web/wwwroot/js/jjmasterdata/jjmasterdata.js
+++ src/Web/wwwroot/js/jjmasterdata/jjmasterdata.js
@@ -2523,6 +2523,11 @@ class TooltipHelper {
         const tooltipTriggerList = document.querySelectorAll(selectorPrefix + ' [data-bs-toggle="tooltip"]');
         tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, { trigger: 'hover' }));
     }
+    static createTooltip(id) {
+        document.addEventListener("DOMContentLoaded", function () {
+            new bootstrap.Tooltip(document.getElementById(id), { trigger: 'hover' });
+        });
+    }
 }
 class UploadAreaListener {
     static configureFileUpload(options) {
