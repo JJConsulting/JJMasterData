a1426ea3c3f52e5541a6cae71372744d090e6e8e
gumbarros <gustavomauriciodebarros@gmail.com>
2024-07-26 11:30:33 -0300

    Merge with preview

--- src/Core/UI/Events/GridDataLoadEventHandler.cs
+++ src/Core/UI/Events/GridDataLoadEventHandler.cs
@@ -3,4 +3,4 @@ using JJMasterData.Core.UI.Events.Args;
 
 namespace JJMasterData.Core.UI.Events;
 
-public delegate Task GridOnDataLoadEventHandler(object sender, GridDataLoadEventArgs e);
+public delegate Task GridDataLoadEventHandler(object sender, GridDataLoadEventArgs e);
