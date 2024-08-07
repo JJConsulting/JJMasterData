e8ff371769e1e47d665b7dc670a6f6a7a374d7cb
Lucio Pelinson <lucio.pelinson@gmail.com>
2024-08-07 17:31:47 -0300

    BugFix: Decimal places

--- src/Core/UI/Components/Controls/TextBox/TextGroupFactory.cs
+++ src/Core/UI/Components/Controls/TextBox/TextGroupFactory.cs
@@ -28,11 +28,6 @@ public class TextGroupFactory(
     {
         var textGroup = Create(field);
 
-        if (field.Component == FormComponent.Currency)
-        {
-            value = value?.ToString()?.Replace(RegionInfo.CurrentRegion.CurrencySymbol, string.Empty).Trim();
-        }
-        
         textGroup.Text = value?.ToString() ?? string.Empty;
         
         return textGroup;
