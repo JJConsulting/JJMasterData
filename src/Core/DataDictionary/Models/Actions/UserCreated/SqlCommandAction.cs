93b3de8c7600fc3dd5f328d43cda3da647a392c6
Lucio Pelinson <lucio.pelinson@gmail.com>
2024-09-17 20:36:52 -0300

    Removed usings

--- src/Core/DataDictionary/Models/Actions/UserCreated/SqlCommandAction.cs
+++ src/Core/DataDictionary/Models/Actions/UserCreated/SqlCommandAction.cs
@@ -1,7 +1,6 @@
 ﻿#nullable enable
-using System.ComponentModel;
+
 using System.ComponentModel.DataAnnotations;
-using Azure.Core;
 using Newtonsoft.Json;
 
 namespace JJMasterData.Core.DataDictionary.Models.Actions;
