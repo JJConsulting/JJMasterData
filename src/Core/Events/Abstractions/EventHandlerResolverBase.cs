9d7e3da3069ddfee8d4b264fe843c594eec3ad9e
gumbarros <gustavomauriciodebarros@gmail.com>
2024-10-15 13:02:33 -0300

    Code quality

--- src/Core/Events/Abstractions/EventHandlerResolverBase.cs
+++ src/Core/Events/Abstractions/EventHandlerResolverBase.cs
@@ -4,17 +4,10 @@ using System.Linq;
 
 namespace JJMasterData.Core.Events.Abstractions;
 
-public class EventHandlerResolverBase<TEventHandler> where TEventHandler : IEventHandler
+public class EventHandlerResolverBase<TEventHandler>(IEnumerable<TEventHandler> eventHandlers) where TEventHandler : IEventHandler
 {
-    private IEnumerable<TEventHandler> EventHandlers { get; }
-
-    protected EventHandlerResolverBase(IEnumerable<TEventHandler> eventHandlers)
-    {
-        EventHandlers = eventHandlers;
-    }
-
     protected TEventHandler? GetEventHandler(string elementName)
     {
-        return EventHandlers.FirstOrDefault(e => e.ElementName == elementName);
+        return eventHandlers.FirstOrDefault(e => e.ElementName == elementName);
     }
 }
