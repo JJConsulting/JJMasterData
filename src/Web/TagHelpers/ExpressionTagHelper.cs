c4c7be15aab26dc5419f0f27e7d43172f18bdcd1
André Sequinel <andresequinel@gmail.com>
2024-11-04 16:02:00 -0300

    Correcao ExpressionTagHelper

--- src/Web/TagHelpers/ExpressionTagHelper.cs
+++ src/Web/TagHelpers/ExpressionTagHelper.cs
@@ -110,7 +110,7 @@ public class ExpressionTagHelper : TagHelper
         if (value is null)
             return input;
         
-        input.WithValue(value);
+        input.WithValue(HttpUtility.HtmlEncode(value));
 
         if (!string.IsNullOrEmpty(Tooltip))
             input.WithToolTip(Tooltip);
