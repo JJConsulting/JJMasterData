b53f7b3282b2e4e202f6807742453b3b41347fd7
gumbarros <gustavomauriciodebarros@gmail.com>
2025-02-20 14:14:10 -0300

    Update FileController.cs routing

--- src/Web/Areas/MasterData/Controllers/FileController.cs
+++ src/Web/Areas/MasterData/Controllers/FileController.cs
@@ -3,17 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JJMasterData.Web.Areas.MasterData.Controllers;
 
-
 public class FileController(ElementFileService service) : MasterDataController
 {
-    [HttpGet]
-    [Route("MasterData/[controller]/{elementName}/{fieldName}/{id}/{fileName}")]
-    [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 14400)]
     public async Task<IActionResult> Index(
-        [FromRoute] string elementName,
-        [FromRoute] string fieldName, 
-        [FromRoute] string id,
-        [FromRoute] string fileName)
+        string elementName,
+        string fieldName, 
+        string id,
+        string fileName)
     {
         var fileStream = await service.GetElementFileAsync(elementName, id, fieldName, fileName);
 
