8e756da1b29da4ffdd4072e5921604ab014d538e
Vanessa Aquino 🌈 <vanessaquinoo@hotmail.com>
2025-08-11 11:08:20 -0300

    Add logging HubDev e ViaCep (#300)
    * Add logging HubDev e ViaCep
    
    * fix: HubDev Constructor
    
    * fix: HubDev Constructor and ViaCep Constructor
    
    * fix: test error
    
    * modified the class names
    
    ---------
    
    Co-authored-by: vanessa.aquino <vanessa.aquino@jjconsulting.com.br>

--- src/Plugins/MasterData.Brasil/Services/ViaCepService.cs
+++ src/Plugins/MasterData.Brasil/Services/ViaCepService.cs
@@ -5,19 +5,25 @@ using JJMasterData.Brasil.Abstractions;
 using JJMasterData.Brasil.Exceptions;
 using JJMasterData.Brasil.Models;
 using JJMasterData.Commons.Util;
+using Microsoft.Extensions.Logging;
 
 namespace JJMasterData.Brasil.Services;
 
-public class ViaCepService(HttpClient httpClient) : ICepService
+public class ViaCepService(HttpClient httpClient, ILogger<ViaCepService> logger) : ICepService
 {
     private const string ViaCepUrl = "https://viacep.com.br/ws/";
 
     public async Task<CepResult> SearchCepAsync(string cep)
     {
+        logger.LogInformation("Searching CEP: {Cep}", cep);
+
         try
         {
             if (string.IsNullOrEmpty(cep))
+            {
+                logger.LogWarning("CEP is null or empty.");
                 throw new ArgumentNullException(nameof(cep));
+            }
 
             var url = $"{ViaCepUrl}{StringManager.ClearCpfCnpjChars(cep)}/json";
 
@@ -25,13 +31,20 @@ public class ViaCepService(HttpClient httpClient) : ICepService
             var content = await response.Content.ReadAsStringAsync();
             var result = CepResult.FromJson(content);
 
-            if (result.Erro)
-                throw new ViaCepException("CEP inválido.");
+            logger.LogInformation("Returnerd by VIACEP for the: {Cep}: {Content}", cep, content);
 
+            if (result.Erro)
+            {
+                logger.LogWarning("CEP {Cep} not found in the ViaCep database.", cep);
+                throw new ViaCepException("Invalid CEP.");
+            }
+
+                logger.LogInformation("CEP {Cep} found successfully.", cep);
             return result;
         }
         catch (Exception ex)
         {
+            logger.LogError(ex, "Error when checking CEP {cep}", cep);
             throw new ViaCepException(ex.Message, ex);
         }
     }
