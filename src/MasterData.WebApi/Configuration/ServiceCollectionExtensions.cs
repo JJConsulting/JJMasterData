49043bbbafa2336c053975a193639b4595959b29
gumbarros <gustavomauriciodebarros@gmail.com>
2025-11-03 15:03:44 -0300

    bugfix: Auto add JsonOptions to minimal apis

--- src/MasterData.WebApi/Configuration/ServiceCollectionExtensions.cs
+++ src/MasterData.WebApi/Configuration/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
+using JJMasterData.Commons.Serialization;
 using JJMasterData.Core.Configuration;
 using JJMasterData.Core.DataDictionary.Services;
 using JJMasterData.WebApi.Models;
 using JJMasterData.WebApi.Services;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace JJMasterData.WebApi.Configuration;
@@ -15,6 +17,12 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
         services.AddScoped<IValidationDictionary, ModelStateWrapper>();
 
+        services.Configure<JsonOptions>(options =>
+        {
+            options.SerializerOptions.PropertyNameCaseInsensitive = true;
+            options.SerializerOptions.Converters.Add(new DictionaryStringObjectJsonConverter());
+        });
+        
         services.AddJJMasterDataCore();
         
         return services;
