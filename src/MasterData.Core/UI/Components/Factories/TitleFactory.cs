3b5f274c19883895bbadac85867acb9da1d9985e
gumbarros <gustavomauriciodebarros@gmail.com>
2026-01-23 11:16:56 -0300

    Fix FormElement strings tracking issue.Fix title l10n. This closes #758.

--- src/MasterData.Core/UI/Components/Factories/TitleFactory.cs
+++ src/MasterData.Core/UI/Components/Factories/TitleFactory.cs
@@ -7,23 +7,27 @@ using JJConsulting.Html.Bootstrap.Models;
 using JJMasterData.Core.DataDictionary.Models;
 using JJMasterData.Core.DataManager.Expressions;
 using JJMasterData.Core.DataManager.Models;
+using Microsoft.Extensions.Localization;
 
 namespace JJMasterData.Core.UI.Components.Factories;
 
-public sealed class TitleFactory(ExpressionsService expressionsService)
+public sealed class TitleFactory(ExpressionsService expressionsService, IStringLocalizer<MasterDataResources> stringLocalizer)
 {
     public JJTitle Create(FormElement formElement, FormStateData formStateData, List<TitleAction>? actions)
     {
         if (formElement == null)
             throw new ArgumentNullException(nameof(formElement));
-    
+
+        var title = expressionsService.GetExpressionValue(formElement.Title, formStateData) as string ?? string.Empty;
+        var subtitle = expressionsService.GetExpressionValue(formElement.SubTitle, formStateData) as string ??
+                       string.Empty;
         var htmlTitle = new JJTitle
         {
-            Title = expressionsService.GetExpressionValue(formElement.Title, formStateData) as string,
+            Title = stringLocalizer[title],
             Size = formElement.TitleSize,
             Icon = formElement.Icon,
             Actions = actions,
-            SubTitle = expressionsService.GetExpressionValue(formElement.SubTitle, formStateData) as string
+            SubTitle = stringLocalizer[subtitle]
         };
 
         return htmlTitle;
