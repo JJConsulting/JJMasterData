6d2b5f8b7e3daa422b28a23813216b6e5df4b25f
gumbarros <gustavomauriciodebarros@gmail.com>
2026-02-19 20:56:21 -0300

    Remove redundant ViewData["FormRouteValues"]

--- src/MasterData.Core/DataDictionary/Models/Actions/ActionSource.cs
+++ src/MasterData.Core/DataDictionary/Models/Actions/ActionSource.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace JJMasterData.Core.DataDictionary.Models.Actions;
 
 public enum ActionSource
@@ -7,4 +8,19 @@ public enum ActionSource
     GridTable = 2,
     Field = 3,
     FormToolbar = 4
+}
+
+public static class ActionSourceExtensions
+{
+    extension(ActionSource source)
+    {
+        public string Name => source switch
+        {
+            ActionSource.GridToolbar => nameof(ActionSource.GridToolbar),
+            ActionSource.GridTable => nameof(ActionSource.GridTable),
+            ActionSource.Field => nameof(ActionSource.Field),
+            ActionSource.FormToolbar => nameof(ActionSource.FormToolbar),
+            _ => throw new ArgumentOutOfRangeException(nameof(source), source, null)
+        };
+    }
 }
