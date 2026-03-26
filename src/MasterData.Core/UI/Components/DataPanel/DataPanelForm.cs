c28fc7ac59faa593dc269ee46cc1e29257a614fe
gumbarros <gustavomauriciodebarros@gmail.com>
2026-03-26 11:51:44 -0300

    Fix JJSearchBox focus on postback due accidental remove at trackpad issue

--- src/MasterData.Core/UI/Components/DataPanel/DataPanelForm.cs
+++ src/MasterData.Core/UI/Components/DataPanel/DataPanelForm.cs
@@ -405,6 +405,7 @@ internal sealed class DataPanelForm
 
         return field.Component switch
         {
+            FormComponent.Search => Scripts.GetReloadPanelScript(field.Name, nameWithPrefix + "_text"),
             FormComponent.CheckBox => Scripts.GetReloadPanelScript(field.Name, nameWithPrefix + "-checkbox"),
             _ =>  Scripts.GetReloadPanelScript(field.Name, nameWithPrefix)
         };
