48998df74b26a45fbf18552d40e27c2052806526
gumbarros <gustavomauriciodebarros@gmail.com>
2026-06-15 09:48:16 -0300

    Add log to protheus call and remove useless plugins

--- src/Plugins/MasterData.Python/Configuration/Options/PythonEngineOptions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace JJMasterData.Python.Configuration.Options;
-
-public class PythonEngineOptions
-{
-    public List<string> AdditionalScriptsPaths { get; } = [];
-    public string ElementScriptsPath { get; set; }
-}
