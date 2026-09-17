d5968edb36fb7c6c7365c369c9467e7229b0ca16
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2026-09-17 09:47:02 -0300

    Use true as default value for IncludeFirstRowAsHeader (#358)

--- src/MasterData.Core/DataManager/Exportation/Abstractions/ExportFormatOptions.cs
+++ src/MasterData.Core/DataManager/Exportation/Abstractions/ExportFormatOptions.cs
@@ -2,8 +2,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace JJMasterData.Core.DataManager.Exportation.Abstractions;
 
-public abstract class ExportFormatOptions 
+public abstract class ExportFormatOptions
 {
-    [Display(Name="Include First Row as Header")]
-    public bool IncludeFirstRowAsHeader { get; set; }
+    [Display(Name = "Include First Row as Header")]
+    public bool IncludeFirstRowAsHeader { get; set; } = true;
 }
